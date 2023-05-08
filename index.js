69b78f17b1263b6b6857d107af821277cf0b56f0
osmangony01 <mdosmangoni376@gmail.com>
2023-05-08 21:33:18 +0600

    perfectly working the server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send('Coffee Store Server is running ...');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Coffee Store Server is running on PORT : ${PORT}`);
+})
